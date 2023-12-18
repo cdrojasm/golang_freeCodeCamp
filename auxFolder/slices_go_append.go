f48af266cfa2ba6a404ffe7f2120c8cc76eb3eb3
cdrojasm <cdrojasm@unal.edu.co>
2023-12-18 14:05:41 -0500

    :wrench: adding go append and append over slice of slices

---
+++ auxFolder/slices_go_append.go
@@ -0,0 +1,63 @@
+package main
+
+import "fmt"
+
+type cost struct {
+	day   int
+	value float64
+}
+
+func getCostsByDay(costs []cost) []float64 {
+	// ?
+	costBuckets := []float64{0}
+	for i := 0; i < len(costs); i++ {
+		cost_i := costs[i]
+		day_i := cost_i.day
+		value_i := cost_i.value
+		if day_i >= len(costBuckets) {
+			lenToAppend := day_i - len(costBuckets) + 1
+			toAppendSlice := make([]float64, lenToAppend)
+			costBuckets = append(costBuckets, toAppendSlice...)
+
+		}
+		costBuckets[day_i] = value_i
+	}
+	return costBuckets
+}
+
+// dont edit below this line
+
+func test(costs []cost) {
+	fmt.Printf("Creating daily buckets for %v costs...\n", len(costs))
+	costsByDay := getCostsByDay(costs)
+	fmt.Println("Costs by day:")
+	for i := 0; i < len(costsByDay); i++ {
+		fmt.Printf(" - Day %v: %.2f\n", i, costsByDay[i])
+	}
+	fmt.Println("===== END REPORT =====")
+}
+
+func main() {
+	test([]cost{
+		{0, 1.0},
+		{1, 2.0},
+		{1, 3.1},
+		{2, 2.5},
+		{3, 3.6},
+		{3, 2.7},
+		{4, 3.34},
+	})
+	test([]cost{
+		{0, 1.0},
+		{10, 2.0},
+		{3, 3.1},
+		{2, 2.5},
+		{1, 3.6},
+		{2, 2.7},
+		{4, 56.34},
+		{13, 2.34},
+		{28, 1.34},
+		{25, 2.34},
+		{30, 4.34},
+	})
+}
