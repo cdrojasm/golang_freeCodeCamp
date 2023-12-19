503665a889121a85fe412bfaae0b649b0263e85c
cdrojasm <cdrojasm@unal.edu.co>
2023-12-18 21:58:29 -0500

    :wrench: adding maps first parts, introduction, mutations, and other topics

---
+++ ch8/slices_go_range.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func indexOfFirstBadWord(msg []string, badWords []string) int {
+
+	for j, word := range msg {
+		for _, badWord := range badWords {
+			if word == badWord {
+				return j
+			}
+		}
+	}
+	return -1
+}
+
+func test(msg []string, badWords []string) {
+	i := indexOfFirstBadWord(msg, badWords)
+	fmt.Printf("Scanning message %v for bad words:\n", msg)
+	for _, badWord := range badWords {
+		fmt.Println(` -`, badWord)
+	}
+	fmt.Printf("Index: %v\n", i)
+	fmt.Println("==================================")
+}
+
+func main() {
+	badWords := []string{"crap", "shoot", "dang", "frick"}
+	message := []string{"hey", "there", "john"}
+	test(message, badWords)
+
+	message = []string{"ugh", "oh", "my", "frick"}
+	test(message, badWords)
+
+	message = []string{"what", "the", "shoot", "I", "hate", "that", "crap"}
+	test(message, badWords)
+}
