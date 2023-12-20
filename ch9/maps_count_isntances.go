719a2d65c06adba54aee199b44448792d483ffc4
cdrojasm <cdrojasm@unal.edu.co>
2023-12-20 13:55:59 -0500

    :wrench: it was completed the maps section of course

--- ch9/maps_count_isntances.go
+++ ch9/maps_count_isntances.go
@@ -1,4 +1,4 @@
-package main 
+package main
 
 import (
 	"crypto/md5"
@@ -6,4 +6,41 @@ import (
 	"io"
 )
 
-func 
+func getCounts(userIDs []string) map[string]int {
+	//?
+	nameCounts := map[string]int{}
+	for _, userID := range userIDs {
+		_, ok := nameCounts[userID]
+		if !ok {
+			nameCounts[userID] = 1
+		} else {
+			nameCounts[userID] += 1
+		}
+	}
+	return nameCounts
+}
+
+func test(userIDs []string, ids []string) {
+	fmt.Printf("Generating counts for %v user IDs...\n", len(userIDs))
+	counts := getCounts(userIDs)
+	fmt.Println("Counts from select IDs:")
+	for _, k := range ids {
+		v := counts[k]
+		fmt.Printf(" - %s: %d\n", k, v)
+	}
+	fmt.Println("======================================")
+}
+
+func main() {
+	userIDs := []string{}
+	for i := 0; i < 10000; i++ {
+		h := md5.New()
+		io.WriteString(h, fmt.Sprint(i))
+		key := fmt.Sprintf("%x", h.Sum(nil))
+		userIDs = append(userIDs, key[:2])
+	}
+
+	test(userIDs, []string{"00", "ff", "dd"})
+	test(userIDs, []string{"aa", "12", "32"})
+	test(userIDs, []string{"bb", "33"})
+}
